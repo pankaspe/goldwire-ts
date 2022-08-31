0c578d963afba0e0ca3659353be4eaa38229ac60
pankaspe <mez.tnt@gmail.com>
2022-08-31 13:55:41 +0200

    porting from bootstrap to chakra ui

--- config/theme.ts
+++ config/theme.ts
@@ -1,5 +1,7 @@
 // theme.ts
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+
 
 // initial color mode config
 const config: ThemeConfig = {
@@ -7,7 +9,26 @@ const config: ThemeConfig = {
   initialColorMode: 'light',
 }
 
+const styles = {
+  global: (props: Record<string, any>) => ({
+    body: {
+      fontFamily: 'body',
+      color: mode('gray.800', 'whiteAlpha.900')(props),
+      bg: mode('white', 'gray.800')(props),
+      lineHeight: 'base',
+    },
+    '*::placeholder': {
+      color: mode('gray.400', 'whiteAlpha.400')(props),
+    },
+    '*, *::before, &::after': {
+      borderColor: mode('gray.200', 'whiteAlpha.300')(props),
+      wordWrap: 'break-word',
+    },
+  }),
+}
+
+
 // extend the theme
-const theme = extendTheme({ config })
+const theme = extendTheme({ config, styles })
 
 export default theme
